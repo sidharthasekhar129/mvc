5ed7e80fe55caff9bd891cab8d8e2ce0544e728e
tinainc <sidhartha_0108@outlook.com>
2021-03-16 09:04:02 +0530

    release 1

---
+++ android/app/src/main/kotlin/com/tina/mvc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tina.mvc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'package:mvc/src/view/MyHomePage.dart';
+
+import 'package:mvc_pattern/mvc_pattern.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
